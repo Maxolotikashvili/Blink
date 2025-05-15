b2770815f039eec2140433d35eb071aa2806e1fa
Maxo Lotikashvili <maxolotikashvili401@gmail.com>
2025-05-15 15:52:53 +0400

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MaterialModule } from '../shared-modules/materia.module';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -16,11 +16,6 @@ import { SigninModalComponent } from '../signin-modal/signin-modal.component';
 export class HomeComponent {
   isModalOpened$!: Observable<boolean>;
 
-  contributorsList: {imageUrl: string, name: string, occupation: string}[] = [
-    {imageUrl: 'maxo-lotikashvili.jpg', name: 'Maxo Lotikashvili', occupation: 'Full-stack developer'},
-    {imageUrl: 'nini-meskhoradze.jpg', name: 'Nini Meskhoradze', occupation: 'Front-end developer'},
-    {imageUrl: 'ani-merabishvili.jpg', name: 'Ani Merabishvili', occupation: 'Mathematician'}
-  ]
   constructor(private modalService: ModalService) {
     this.isModalOpened$ = modalService.isModalOpened$;
   }
